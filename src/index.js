7545c0dc333c181a1bcf9c1653afd38241618287
Danyl Fernandes <realllydan@gmail.com>
2021-09-09 21:06:05 +0530

    :construction: Add GA4React

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import GA4React from "ga-4-react";
 import App from "./App";
 
-ReactDOM.render(<App />, 
-    document.getElementById("root")
-);
+const ga4react = new GA4React('G-P3SFV0S4LJ');
+
+(async (_) => {
+  await ga4react
+    .initialize()
+    .then((res) => console.log("Successfully started."))
+    .catch((err) => console.log("Failure starting."))
+    .finally(() => {
+      ReactDOM.render(<App />, document.getElementById("root"));
+    });
+})();
