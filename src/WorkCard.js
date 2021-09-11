d777ffe28274a8b418994677048d3fd5b7bfd842
Danyl Fernandes <realllydan@gmail.com>
2021-09-11 13:29:05 +0530

    Add work-type expansion on hover

--- src/WorkCard.js
+++ src/WorkCard.js
@@ -1,32 +1,48 @@
-import "./styles/WorkCard.css"
+import "./styles/WorkCard.css";
 
 const WorkCard = ({ data }) => {
-    const title = data.subject;
-    const subtitle = data.date_due;
-    const type = getParsedWorkType(data.type);
+  const title = data.subject;
+  const subtitle = data.date_due;
+  const type = getParsedWorkType(data.type);
+  const indicatorText = getIndicatorText(type);
 
-    return (
-        <div className="wrapper">
-            <div className={"indicator " + type}></div>
-            <div className="details">
-                <p className="title">{title}</p>
-                <p className="subtitle">{subtitle}</p>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="wrapper">
+      <div className={"indicator " + type}>
+        <p>{indicatorText}</p>
+      </div>
+      <div className="details">
+        <p className="title">{title}</p>
+        <p className="subtitle">{subtitle}</p>
+      </div>
+    </div>
+  );
+};
 
 function getParsedWorkType(type) {
-    switch (type) {
-        case "Assignment":
-            return "assignment";
-        case "Experiment":
-            return "experiment";
-        case "Quiz":
-            return "quiz";
-        default:
-            return "other";
-    }
+  switch (type) {
+    case "Assignment":
+      return "assignment";
+    case "Experiment":
+      return "experiment";
+    case "Quiz":
+      return "quiz";
+    default:
+      return "other";
+  }
+}
+
+function getIndicatorText(type) {
+  switch (type) {
+    case "assignment":
+      return "A";
+    case "experiment":
+      return "E";
+    case "quiz":
+      return "Q";
+    default:
+      return "O";
+  }
 }
 
 export default WorkCard;
