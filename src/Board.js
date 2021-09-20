9835355de847e082c01d404681e2670b949bcba8
Danyl Fernandes <androidan1304@gmail.com>
2021-09-20 16:17:33 +0530

    :sparkles: Add stateful Modal to Board

--- src/Board.js
+++ src/Board.js
@@ -1,14 +1,35 @@
-import CourseWorkList from './CourseWorkList';
+import { useState } from 'react';
+import WorkCard from './WorkCard';
+import Modal from './Modal';
 import './styles/Board.css';
 
 const Board = ({ name, date, workData }) => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [modalData, setModelData] = useState({});
+
     return (
         <div className="board">
             {date && <h1 className="board-date">{date}</h1>}
             <h2 className="board-title">{name}</h2>
             <div className="board-list">
-                <CourseWorkList data={workData} />
+                {workData.map(work => {
+                    return (
+                        <WorkCard
+                            key={work.id}
+                            data={work}
+                            onCardClick={() => {
+                                setModelData(work);
+                                setIsModalOpen(true);
+                            }}
+                        />
+                    );
+                })}
             </div>
+            <Modal
+                data={modalData}
+                isOpen={isModalOpen}
+                onClose={() => setIsModalOpen(false)}
+            />
         </div>
     );
 };
