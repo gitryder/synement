2595fd305aab360272daced869becc58c5ac65c7
Danyl Fernandes <androidan1304@gmail.com>
2021-09-20 19:33:04 +0530

    :ribbon: Add due-date to modal body

--- src/Modal.js
+++ src/Modal.js
@@ -1,5 +1,5 @@
 import './styles/Modal.css';
-import {getAbbreviation} from './utils';
+import { getAbbreviation } from './utils';
 
 const Modal = ({ data, isOpen, onClose }) => {
     if (!isOpen) return null;
@@ -8,17 +8,24 @@ const Modal = ({ data, isOpen, onClose }) => {
             <div className="overlay"></div>
             <div className="modal">
                 <div className="modal-header">
-                    <p className="modal-title">{getAbbreviation(data.subject)}</p>
+                    <p className="modal-title">
+                        {getAbbreviation(data.subject)}
+                    </p>
                     <button
                         className="modal-close-btn"
                         id="modal-close-btn"
-                        onClick={onClose}>
+                        onClick={onClose}
+                    >
                         &times;
                     </button>
                 </div>
                 <div class="modal-body">
                     <strong>Type:</strong>
                     <p>{data.type}</p>
+                    <br />
+
+                    <strong>Due date:</strong>
+                    <p>{data.date_due}</p>
                     <br />
 
                     <strong>Title: </strong>
